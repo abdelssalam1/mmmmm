2e1743cc90978cdd73898153fcf4b46bfc2d1cf1
abdelssalam1 <38702059+abdelssalam1@users.noreply.github.com>
2018-11-29 14:16:32 +0200

    adding new files

---
+++ page1.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+	</head>
+	<body>
+		<h1>Enter Your Information:</h1>
+		
+		<form action="page2.php" method="post">
+			<input name="name" type="text" placeholder="Enter Your Name" />
+			<input name="mobile" type="text" placeholder="Enter Your Mobile Number" />
+			<input type="submit" value="Next Page"/>
+		</form>
+		
+	</body>
+</html>
